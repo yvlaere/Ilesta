b1f4060f3e0800a7f15d796541d094047b412667
yvlaere <yari.vanlaere@gmail.com>
2026-03-26 21:08:18 +0100

    change genome size parameter name

--- src/cli.rs
+++ src/cli.rs
@@ -52,7 +52,7 @@ pub struct AlignReadsArgs {
 
     /// Optional input genome size (if not provided, will be estimated from data)
     #[arg(long)]
-    pub input_genome_size: Option<u32>,
+    pub genome_size: Option<u32>,
 }
 
 impl From<&AlignReadsArgs> for crate::configs::AlignReadsConfig {
@@ -64,7 +64,7 @@ impl From<&AlignReadsArgs> for crate::configs::AlignReadsConfig {
             paf: args.paf.clone(),
             min_read_length: args.min_read_length,
             min_base_quality: args.min_base_quality,
-            input_genome_size: args.input_genome_size,
+            genome_size: args.genome_size,
         }
     }
 }
@@ -152,7 +152,7 @@ pub struct AssembleArgs {
 
     /// Optional input genome size (if not provided, will be estimated from data)
     #[arg(long)]
-    pub input_genome_size: Option<u32>,
+    pub genome_size: Option<u32>,
 
     /// Alignment filtering parameters (optional if --overlaps is provided)
 
@@ -216,7 +216,7 @@ impl From<&AssembleArgs> for crate::configs::AssembleConfig {
             paf: args.paf.clone(),
             min_read_length: args.min_read_length,
             min_base_quality: args.min_base_quality,
-            input_genome_size: args.input_genome_size,
+            genome_size: args.genome_size,
 
             // alignment filtering
             min_overlap_length: args.min_overlap_length,
